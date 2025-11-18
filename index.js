199c25768ab1df63ea365d602bef71bc85ea7f96
shahab hamidi <shahabhamidi73@gmail.com>
2025-11-18 15:11:41 +0430

    Update index.js

--- index.js
+++ index.js
@@ -83,8 +83,8 @@ async function saveData(data) {
 }
 async function loadUsers() {
   try {
-    const stored = localStorage.getItem(usersStorageKey);
-    allUsers = stored ? JSON.parse(stored) : [];
+const stored = localStorage.getItem(usersStorageKey);
+allUsers = stored && stored !== 'undefined' ? JSON.parse(stored) : [];
     if (allUsers.length === 0) {
       const defaultAdmin = {
         __backendId: generateId(),
@@ -1405,4 +1405,5 @@ function toggleUserDropdown() {
 document.addEventListener('DOMContentLoaded', initApp);
 // if (window.location.hostname !== '127.0.0.1' && window.location.hostname !== 'localhost') {
 //   (function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'99bbf8eb8072d381',t:'MTc2MjY3NzI4MC4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();
+
 // }
